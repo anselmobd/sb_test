83127bf3130501c49ecb7e8b9ab9e8b4cf4c6225
anselmobd <anselmo.blanco.dominguez@gmail.com>
2023-09-21 12:03:14 -0300

    continuando o rename de demo para sb_test

--- src/test/java/com/example/sb_test/DemoApplicationTests.java
+++ src/test/java/com/example/sb_test/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.sb_test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SBTestApplicationTests {
 
 	@Test
 	void contextLoads() {
