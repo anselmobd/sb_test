a977f869559d7921106c3ad8e9681c55ca1c2330
anselmobd <anselmo.blanco.dominguez@gmail.com>
2023-09-21 11:09:18 -0300

    Start!

---
+++ src/main/java/com/example/demo/SBTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBTestApplication.class, args);
+	}
+
+}
